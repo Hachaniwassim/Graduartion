8a43a273317e5e1ff6c0ca5e30cca9ca95e89693
hachaniwassim <wassimhach16@gmail.com>
2022-03-31 11:18:20 +0100

    Generation components and fixing routing problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from "@angular/forms";
 import { CommonModule } from '@angular/common';
+import { MainTemplateComponent } from "./main-template/main-template.component";
+import { HomeComponent } from "./home/home.component";
 
 
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+{ path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+  ],
+  imports: [RouterModule.forRoot(routes),FormsModule,CommonModule],
+  exports: [RouterModule],
+
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LeftSlideBarComponent } from './left-slide-bar/left-slide-bar.component';
 import { MainTemplateComponent } from './main-template/main-template.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LeftSlideBarComponent,
-    MainTemplateComponent
+
   ],
   imports: [
     BrowserModule,
