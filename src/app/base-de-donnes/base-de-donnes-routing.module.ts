23e9ff402334e9ac45843e390e3bb9e5d62357ee
hachaniwassim <wassimhach16@gmail.com>
2022-04-04 00:54:13 +0100

    support component

--- src/app/base-de-donnes/base-de-donnes-routing.module.ts
+++ src/app/base-de-donnes/base-de-donnes-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseDeDonnesComponent } from './base-de-donnes.component';
-
+import { MaterialModule } from '../material/material.module';
 const routes: Routes = [{ path: '', component: BaseDeDonnesComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes),MaterialModule],
+  exports: [RouterModule,MaterialModule]
 })
 export class BaseDeDonnesRoutingModule { }
--- src/app/gestion/gestion-routing.module.ts
+++ src/app/gestion/gestion-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GestionComponent } from './gestion.component';
-
+import { MaterialModule } from '../material/material.module';
 const routes: Routes = [{ path: '', component: GestionComponent }];
 
+
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes),MaterialModule],
+  exports: [RouterModule,MaterialModule]
 })
 export class GestionRoutingModule { }
