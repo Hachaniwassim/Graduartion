1700739a8f2449596f48488d4f2d7be5df652b92
hachaniwassim <wassimhach16@gmail.com>
2022-04-04 01:44:56 +0100

    read all user component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { users } from './user';
+import { user } from './user.model';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  public usersList: user[] = users;
+
 
   constructor() { }
 
