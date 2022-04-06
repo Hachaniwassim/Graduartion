eec5ed9f164ceaee26dbd5d1035ea8b41fbd997f
hachaniwassim <wassimhach16@gmail.com>
2022-04-06 16:20:41 +0100

    general configrations component

--- src/app/configurations/configurations.component.ts
+++ src/app/configurations/configurations.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { configuration } from './configurations.model';
+import { configurations } from './configurations';
 @Component({
   selector: 'app-configurations',
   templateUrl: './configurations.component.html',
   styleUrls: ['./configurations.component.css']
 })
 export class ConfigurationsComponent implements OnInit {
+
+  public cofigrationsList: configuration[] = configurations;
 
   constructor() { }
 
