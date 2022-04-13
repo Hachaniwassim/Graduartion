59e932785ce1ae9227b6cf33aee10c0e0877c43b
hachaniwassim <wassimhach16@gmail.com>
2022-04-13 16:29:01 +0100

    fixing titles and street map

--- src/app/whoarewe/whoarewe.component.ts
+++ src/app/whoarewe/whoarewe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const L : any ;
 
 @Component({
   selector: 'app-whoarewe',
@@ -9,7 +10,54 @@ export class WhoareweComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const latLong = [coords.latitude, coords.longitude];
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+     let mymap = L.map('map').setView(latLong, 13);
 
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3VicmF0MDA3IiwiYSI6ImNrYjNyMjJxYjBibnIyem55d2NhcTdzM2IifQ.-NnMzrAAlykYciP4RP9zYQ',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token',
+        }
+      ).addTo(mymap);
+
+      let marker = L.marker(latLong).addTo(mymap);
+    });
+    this.watchPosition();}
+    watchPosition() {
+      let desLat = 0;
+      let desLon = 0;
+      let id = navigator.geolocation.watchPosition(
+        (position) => {
+          console.log(
+            `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+          );
+          if (position.coords.latitude === desLat) {
+            navigator.geolocation.clearWatch(id);
+          }
+        },
+        (err) => {
+          console.log(err);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        }
+      );
+    }
 }
