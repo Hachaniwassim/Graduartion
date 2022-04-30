b7e02f2cd563d61118733b388572ed712fb06075
hachaniwassim <wassimhach16@gmail.com>
2022-04-30 16:17:21 +0100

    adding account view , including products

--- back-office/src/app/_services/products.service.ts
+++ back-office/src/app/_services/products.service.ts
@@ -29,56 +29,56 @@ export class ProductService {
   }
   constructor(private http :HttpClient, private datePipe: DatePipe) { }
 
-  //http opttion
-  httpOptions={ 
-    headers:new HttpHeaders({
-      'content-type':'application/json'
+//http opttion
+httpOptions={ 
+  headers:new HttpHeaders({
+    'content-type':'application/json'
 
-    })
-  }
-  //handel api  errors 
-  handleError(error: HttpErrorResponse){
-    if( error.error instanceof ErrorEvent){
-    //a client-side or a neetwork error occurend .Handel it accordingly
-    console.error('An Error occurend' , error.error.message)
+  })
+}
+//handel api  errors 
+handleError(error: HttpErrorResponse){
+  if( error.error instanceof ErrorEvent){
+  //a client-side or a neetwork error occurend .Handel it accordingly
+  console.error('An Error occurend' , error.error.message)
 
-  }
-  else{
-    // the backend may returned an successfully response code 
-    // the response body may contain clues as to what went wrong 
-    console.error(`backend returned code ${error.status}, ` +
-    `body was : ${ error.error}`
-    );}
-   // return an observabel with a user-facing error message 
-  return throwError( 'something bad happined , please try again later .');
+}
+else{
+  // the backend may returned an successfully response code 
+  // the response body may contain clues as to what went wrong 
+  console.error(`backend returned code ${error.status}, ` +
+  `body was : ${ error.error}`
+  );}
+ // return an observabel with a user-facing error message 
+return throwError( 'something bad happined , please try again later .');
 };
 
 
 // insert 
-createProduct(item : productsDTO):Observable<productsDTO>{
-  return this.http.post<productsDTO>(this.base_url,JSON.stringify(item),this.httpOptions).pipe(retry(2),catchError(this.handleError));
+createCompayBusiness(item : productsDTO):Observable<productsDTO>{
+return this.http.post<productsDTO>(this.base_url,JSON.stringify(item),this.httpOptions).pipe(retry(2),catchError(this.handleError));
 }
 
-//get all account data 
-getAllProduct():Observable<productsDTO>{
-   return this.http.get<productsDTO>(this.base_url).pipe(retry(2),catchError(this.handleError));
+//get all team data 
+getAllCompanyBussiness():Observable<productsDTO>{
+ return this.http.get<productsDTO>(this.base_url).pipe(retry(2),catchError(this.handleError));
+}
+
+
+// get team by id
+getByidCompany(id:number):Observable<productsDTO>{
+  return this.http.get<productsDTO>(this.base_url + '/' +id).pipe(retry(2),catchError(this.handleError));
+
+}
+
+ // update team by Id the
+ updateCompanyBusiness(item : productsDTO){
+  return this.http.put<productsDTO>(this.base_url,JSON.stringify(item),this.httpOptions).pipe(retry(2),catchError(this.handleError));
  }
 
-
-  // get product by id
-  getByidProduct(id:number):Observable<productsDTO>{
-    return this.http.get<productsDTO>(this.base_url + '/' +id).pipe(retry(2),catchError(this.handleError));
-
-  }
-
-   // update product by Id the
-   updateProduct(item : productsDTO){
-    return this.http.put<productsDTO>(this.base_url,JSON.stringify(item),this.httpOptions).pipe(retry(2),catchError(this.handleError));
-   }
-
-    // delete products
-    deleteProduct(id:number){
-      return this.http.delete<productsDTO>(this.base_url + '/' +id,this.httpOptions).pipe(retry(2),catchError(this.handleError));
+  // delete cars
+  deleteCompanyBusiness(id:number){
+    return this.http.delete<productsDTO>(this.base_url + '/' +id,this.httpOptions).pipe(retry(2),catchError(this.handleError));
 
 }
 
