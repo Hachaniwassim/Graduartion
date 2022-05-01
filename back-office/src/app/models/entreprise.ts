54df59f54f938d8a620bc6f5757e5cc8ba2470df
abirtarchoun <abirtarchoun.8@gmail.com>
2022-05-01 04:57:35 +0100

    affiche liste entreprise

--- back-office/src/app/models/entreprise.ts
+++ back-office/src/app/models/entreprise.ts
@@ -3,16 +3,15 @@ import { Groupe } from "./groupe";
 export interface Entrepris{
     id?: number;
     companyname?: string;
-    fiscalCode?: string;
+    codefiscale?: string;
     adresse?: string;
     phone: string;
     fax: string;
     email: string;
-    contact : string;
     note: string;
     createdDate :Date,
     lastModifiedDate:Date,   
-    groupe ?: Groupe[];
+    //groupe ?: Groupe[];
    
   
 
