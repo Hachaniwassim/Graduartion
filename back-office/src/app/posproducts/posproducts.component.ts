178dd6303418d2a449692cc57c744f6d60d6870d
hachaniwassim <wassimhach16@gmail.com>
2022-05-01 16:38:05 +0100

    preparing products list with get and update still.

--- back-office/src/app/posproducts/posproducts.component.ts
+++ back-office/src/app/posproducts/posproducts.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { poscontactes } from '../poscontacts/poscontacts';
-import { poscontacts } from '../poscontacts/poscontactes.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { productsDTO } from 'src/app/models/dto/productsDTO';
+import { DialogService } from 'src/app/shared/dialog.service';
+import { NotificationService } from 'src/app/shared/notification.service';
+import { ProductService } from 'src/app/_services/products.service';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-posproducts',
   templateUrl: './posproducts.component.html',
   styleUrls: ['./posproducts.component.css']
 })
 export class PosproductsComponent implements OnInit {
-  public posproductslist: poscontacts[] = poscontactes;
-
-  constructor() { }
+  public productlist: any = [];
+  constructor( private teamsService : ProductService) { }
 
   ngOnInit(): void {
+    this.teamsService.getAllCompanyBussiness().subscribe(
+      res => this.productlist = res
+    );
   }
 
 }
