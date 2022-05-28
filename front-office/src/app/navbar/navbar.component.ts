6f42e90b7b7a1430e8e57797d8b66caaa0b2128f
hachaniwassim <wassimhach16@gmail.com>
2022-05-28 03:05:19 +0100

    navbar/login fixes

--- front-office/src/app/navbar/navbar.component.ts
+++ front-office/src/app/navbar/navbar.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {Location}from '@angular/common';
-
+import { TokenStorageService } from '../_services/token-storage.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  showUserBoard = false ;
+  entrepriseSelected = false;
+  username?: string;
 
-  constructor(private router : Router, public _location: Location) { }
 
-  ngOnInit(): void {
-  }
+  constructor(private tokenStorageService: TokenStorageService,private router : Router, public _location: Location) { }
+
+
     //refrech 
     refresh(): void {
       this.router.navigateByUrl("/refresh", { skipLocationChange: true }).then(() => {
@@ -26,6 +33,27 @@ export class NavbarComponent implements OnInit {
         window.location.reload();
       }, 1000);
   }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.showUserBoard = this.roles.includes('ROLE_USER')
+      this.username = user.username;
+      this.entrepriseSelected = localStorage.getItem('idEntreprise') ? true:false
+    }
+   
+  }
+  logout(): void {
+   this.tokenStorageService.signOut();
+    window.location.reload();
+    this.refresh();
+  }
+
   
 
 }
