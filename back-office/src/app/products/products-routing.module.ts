af49e4909e706017f56a7ed5a40aeda99398ad27
hachaniwassim <wassimhach16@gmail.com>
2022-05-29 19:29:00 +0100

    fixing product component on back-office

--- back-office/src/app/products/products-routing.module.ts
+++ back-office/src/app/products/products-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CKEditorModule } from 'ckeditor4-angular';
 
 const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forChild(routes),
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
+  ],
+  exports: [
+    RouterModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
+  ]
 })
 export class ProductsRoutingModule { }
