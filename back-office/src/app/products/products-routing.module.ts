073b2cd5964b3dbba2d82ca96e6b03be5e0adf72
abirtarchoun <abirtarchoun.8@gmail.com>
2022-05-29 21:21:35 +0100

    update

--- back-office/src/app/products/products-routing.module.ts
+++ back-office/src/app/products/products-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { MaterialModule } from '../material/material.module';
 import { ProductsComponent } from './products.component';
 
 const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes),MaterialModule,ReactiveFormsModule,FormsModule],
-  exports: [RouterModule, MaterialModule,ReactiveFormsModule,FormsModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class ProductsRoutingModule { }
