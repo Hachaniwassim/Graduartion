84e2d065dbffa843e9e44237ffce78991b591ab0
hachaniwassim <wassimhach16@gmail.com>
2022-05-30 01:26:26 +0100

    some fixes

--- front-office/src/app/products/products-routing.module.ts
+++ front-office/src/app/products/products-routing.module.ts
@@ -3,14 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes) , MaterialModule],
-  exports: [RouterModule,    MaterialModule  ],
+  imports: [RouterModule.forChild(routes) , 
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+  
+  ],
+  exports: [RouterModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+
+        ],
   declarations: [
     ProductDetailsComponent,
   ]
+  
 })
 export class ProductsRoutingModule { }
